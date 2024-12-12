fea3b9b4ebd835056dd6a7b97ce1a475ddc64fa9
appletox <jchen167@stevens.edu>
2024-12-12 15:43:45 -0500

    fixed create-post frontend JS

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,13 +1,20 @@
-post = document.getElementById("create-post");
-
 (() => {
+  let post = document.getElementById("create-post");
   if (post) {
-    postType = post.getElementById("post-type");
-    if ((postType.value = "route")) {
-      startDiv = post.getElementById("start-div");
-      endDiv = post.getElementById("end-div");
-      startDiv.style.display = "none";
-      endDiv.style.display = "none";
-    }
+    post.addEventListener("change", (event) => {
+      let postType = document.getElementById("post-type");
+      let startDiv = document.getElementById("start-div");
+      let endDiv = document.getElementById("end-div");
+      console.log("before if");
+      if (postType.value == "route") {
+        console.log("in if");
+        startDiv.style.display = "none";
+        endDiv.style.display = "none";
+      } else {
+        console.log("in else");
+        startDiv.style.display = "block";
+        endDiv.style.display = "block";
+      }
+    });
   }
 })();
